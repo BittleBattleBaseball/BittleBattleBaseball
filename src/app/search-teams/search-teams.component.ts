2be698c24a7963cbc7b07f5033e90c5a351d39cc
Ian Kelly <BIttleBattleBaseball@gmail.com>
2024-09-05 15:59:46 -0600

    Add filter year drop-downs to select other years faster

--- src/app/search-teams/search-teams.component.ts
+++ src/app/search-teams/search-teams.component.ts
@@ -1,68 +1,94 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { TeamSearchResultViewModel } from '../team-search-result-view-model';
-import { SearchTeamsServiceService } from '../search-teams-service.service';
-import { NewGameSetupViewModel } from '../new-game-setup-view-model';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { TeamSearchResultViewModel } from "../team-search-result-view-model";
+import { SearchTeamsServiceService } from "../search-teams-service.service";
+import { NewGameSetupViewModel } from "../new-game-setup-view-model";
 
 @Component({
-  selector: 'app-search-teams',
-  templateUrl: './search-teams.component.html',
-  styleUrls: ['./search-teams.component.scss']
+  selector: "app-search-teams",
+  templateUrl: "./search-teams.component.html",
+  styleUrls: ["./search-teams.component.scss"],
 })
 export class SearchTeamsComponent implements OnInit {
   GameSetup: NewGameSetupViewModel;
   SearchableSeasons: number[] = new Array<number>();
   SearchingHomeTeams: boolean;
   SearchingAwayTeams: boolean;
-  constructor(private router: Router, private searchTeamsService: SearchTeamsServiceService) {
+  constructor(
+    private router: Router,
+    private searchTeamsService: SearchTeamsServiceService
+  ) {
     this.GameSetup = new NewGameSetupViewModel();
-    this.GameSetup.League = 'mlb';
+    this.GameSetup.League = "mlb";
     let today = new Date();
     for (let i: number = today.getFullYear() - 1; i > 1875; i--) {
       this.SearchableSeasons.push(i);
     }
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   setupNewSinglePlayerGame() {
     this.router.navigateByUrl("searchteams");
   }
 
   SetLineups() {
-
-    let newGameId = ((new Date().getTime() * 10000) + 621355968000000000);
-    localStorage.setItem('game_setup_' + newGameId, JSON.stringify(this.GameSetup));
+    let newGameId = new Date().getTime() * 10000 + 621355968000000000;
+    localStorage.setItem(
+      "game_setup_" + newGameId,
+      JSON.stringify(this.GameSetup)
+    );
 
     this.router.navigateByUrl("/setlineups/" + newGameId);
   }
 
   setDH() {
-    this.GameSetup.IsDesignatedHitterEnabled = !this.GameSetup.IsDesignatedHitterEnabled;
+    this.GameSetup.IsDesignatedHitterEnabled =
+      !this.GameSetup.IsDesignatedHitterEnabled;
   }
 
-  HomeSearchResults: TeamSearchResultViewModel[] = new Array<TeamSearchResultViewModel>();
+  HomeSearchResults: TeamSearchResultViewModel[] =
+    new Array<TeamSearchResultViewModel>();
 
-  AwaySearchResults: TeamSearchResultViewModel[] = new Array<TeamSearchResultViewModel>();
+  AwaySearchResults: TeamSearchResultViewModel[] =
+    new Array<TeamSearchResultViewModel>();
+
+    SelectedHomeYear: number = undefined;
+    SelectedAwayYear: number = undefined;
 
   SearchTeams(isHomeTeam: boolean, year: number) {
     if (isHomeTeam) {
       this.SearchingHomeTeams = true;
       this.HomeSearchResults = [];
-      this.searchTeamsService.GetTeamsBySeason(this.GameSetup.League, year).subscribe(results => {
-        this.HomeSearchResults = results;
+      this.SelectedHomeYear = undefined;
+      if (year != -1) {
+        this.searchTeamsService
+          .GetTeamsBySeason(this.GameSetup.League, year)
+          .subscribe((results) => {
+            this.HomeSearchResults = results;
+            this.SearchingHomeTeams = false;
+            this.SelectedHomeYear = year;
+          });
+      }
+      else{
         this.SearchingHomeTeams = false;
-      });
-
-    }
-    else {
+      }
+    } else {
       this.SearchingAwayTeams = true;
       this.AwaySearchResults = [];
-      this.searchTeamsService.GetTeamsBySeason(this.GameSetup.League, year).subscribe(results => {
-        this.AwaySearchResults = results;
+      this.SelectedAwayYear = undefined;
+      if (year != -1) {
+      this.searchTeamsService
+        .GetTeamsBySeason(this.GameSetup.League, year)
+        .subscribe((results) => {
+          this.AwaySearchResults = results;
+          this.SearchingAwayTeams = false;
+          this.SelectedAwayYear = year;
+        });
+      }
+      else{
         this.SearchingAwayTeams = false;
-      });
+      }
     }
   }
 
@@ -82,6 +108,6 @@ export class SearchTeamsComponent implements OnInit {
   }
 
   scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }
 }
