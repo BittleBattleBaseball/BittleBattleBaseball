831a017cf7edc22f6fe5cf6ec27def0383b2d47c
Ian Kelly <BIttleBattleBaseball@gmail.com>
2024-09-14 09:42:21 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  API_URL: "https://localhost:5001/api/"
-  //API_URL: "https://bittlebattlebaseballwebapi.onrender.com/api/"
+  //API_URL: "https://localhost:5001/api/"
+  API_URL: "https://bittlebattlebaseballwebapi.onrender.com/api/"
 };
 
 /*
